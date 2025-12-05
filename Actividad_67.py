10047b503290f7f679f9c2aba5244749f1ef300d
Gio Antonio Canto Gómez <gioantonio98@gmail.com>
2025-12-05 17:24:12 -0600

    Update Actividad_67.py

--- Actividad_67.py
+++ Actividad_67.py
@@ -8,8 +8,7 @@ def main():
     etiqueta1 = tk.Label(window, text="Haz clic en cualquier parte de la ventana", font=("Arial", 11))
     etiqueta1.pack(pady=20)
 
-    # Segunda etiqueta fija al final
-    etiqueta2 = tk.Label(window, text="¡⁶🤷⁷!", font=("Arial", 11))
+    etiqueta2 = tk.Label(window, text="¡⁶🤷⁷!", font=("Arial", 11)) #⁶🤷‍♂️⁷
     etiqueta2.pack(side="bottom", pady=10)
 
     def handle_click(event):
