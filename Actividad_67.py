9d99efa4b6dd4195f8c74dd7cdbaefde7a1f1772
Gio Antonio Canto Gómez <gioantonio98@gmail.com>
2025-12-05 17:19:03 -0600

    ¡67!

--- Actividad_67.py
+++ Actividad_67.py
@@ -1,21 +1,24 @@
 import tkinter as tk
 
-
 def main():
     window = tk.Tk()
     window.title("Eventos con clic del ratón")
     window.geometry("400x200")
 
-    etiqueta = tk.Label(window, text="Haz clic en cualquier parte de la ventana", font=("Arial", 11))
-    etiqueta.pack(pady=40)
+    etiqueta1 = tk.Label(window, text="Haz clic en cualquier parte de la ventana", font=("Arial", 11))
+    etiqueta1.pack(pady=20)
+
+    # Segunda etiqueta fija al final
+    etiqueta2 = tk.Label(window, text="¡⁶🤷⁷", font=("Arial", 11))
+    etiqueta2.pack(side="bottom", pady=10)
 
     def handle_click(event):
-        etiqueta.config(text=f"Clic en x={event.x}, y={event.y}")
+        etiqueta1.config(text=f"Clic en x={event.x}, y={event.y}")
 
     window.bind("<Button-1>", handle_click)
 
     window.mainloop()
 
-
 if __name__ == "__main__":
     main()
+
